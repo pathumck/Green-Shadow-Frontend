c33275c7cb690010dd16e47b4ff9ea72609c66d5
pathumck <ckpathumck@gmail.com>
2024-12-01 18:42:37 +0530

    create update a crop

--- controllers/crop_controller.js
+++ controllers/crop_controller.js
@@ -39,7 +39,8 @@ $("#btn_save_crop").click(function() {
      $('#category_modal_crop').val('');
      $('#season_modal_crop').val('');
      $('#image_modal_crop').val('');
-     $('#image_preview_modal_crop').attr('src', '').hide();
+     $('#imagePreview_modal_crop').attr('src', '').hide();
+     $('#imagePreview_modal_crop').hide();
      $('#modal_crop').modal('hide');
      loadCropCodeToModel();
      loadCropDetailsTable();
@@ -68,7 +69,7 @@ function loadCropDetailsTable() {
             <td>${data.category}</td>
             <td>${data.season}</td>
             <td><img src="data:image/jpeg;base64,${data.image}" alt="Image" class="image-container"></td>
-            <td><button type="button" class="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#modal_crop"
+            <td><button type="button" class="btn btn-primary btn-update-crop" data-bs-toggle="modal" data-bs-target="#modal_crop_update"
             data-crop-code="${data.cropCode}"
             data-common-name="${data.commonName}"
             data-scientific-name="${data.scientificName}"
@@ -76,15 +77,20 @@ function loadCropDetailsTable() {
             data-season="${data.season}"
             data-image="data:image/jpeg;base64,${data.image}"
             >Update</button></td>
-            <td><button type="button" class="btn btn-danger btn-delete" data-crop-code="${data.cropCode}">Delete</button></td>
+            <td><button type="button" class="btn btn-danger btn-delete-crop" data-crop-code="${data.cropCode}">Delete</button></td>
           </tr>
         `;
         tableBody.append(row);
       });
 
-      $('.btn-delete').on('click', function() {
+      $('.btn-delete-crop').on('click', function() {
         const cropCode = $(this).data('crop-code');
         deleteCrop(cropCode);
+      });
+
+      $('.btn-update-crop').on('click', function() {
+        const cropCode = $(this).data('crop-code');
+        loadUpdateCropModal(cropCode);
       });
     },
     error: function(error) {
@@ -110,3 +116,65 @@ function deleteCrop(cropCode) {
     });
   }
 }
+
+function loadUpdateCropModal(cropCode) {
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/crop/' + cropCode,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      $('#code_modal_crop_update').text(data.data.cropCode);
+      $('#commonName_modal_crop_update').val(data.data.commonName);
+      $('#scientificName_modal_crop_update').val(data.data.scientificName);
+      $('#category_modal_crop_update').val(data.data.category);
+      $('#season_modal_crop_update').val(data.data.season);
+      const imageBase64 = data.data.image;
+      $('#existing_image_crop').attr('value', imageBase64);
+      $('#imagePreview_modal_crop_update').attr('src', 'data:image/jpeg;base64,' + imageBase64).show();
+      $('#modal_crop_update').modal('show');
+    },
+    error: function(error) {
+      alert(error.responseText);
+    }
+  });
+}
+
+$('#btn_update_crop').on('click', function() {
+  const formData = new FormData();
+  formData.append('cropCode', $('#code_modal_crop_update').text());
+  formData.append('commonName', $('#commonName_modal_crop_update').val());
+  formData.append('scientificName', $('#scientificName_modal_crop_update').val());
+  formData.append('category', $('#category_modal_crop_update').val());
+  formData.append('season', $('#season_modal_crop_update').val());
+  const image = $('#image_modal_crop_update')[0].files[0]; formData.append('image', $('#image_modal_crop_update')[0].files[0]);
+  const existingImage = $('#existing_image_crop').attr('value');
+  if (image) {
+    formData.append('image', image);
+  } else {
+    formData.append('image', convertBase64StringToFile(existingImage));
+  }
+
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/crop',
+    type: 'PUT',  
+    data: formData,     
+    contentType: false,             
+    processData: false,     
+    success: function(data) {
+      alert("Crop updated successfully");
+      $('#commonName_modal_crop_update').val('');
+      $('#scientificName_modal_crop_update').val('');
+      $('#category_modal_crop_update').val('');
+      $('#season_modal_crop_update').val('');
+      $('#image_modal_crop_update').val('');
+      $('#imagePreview_modal_crop_update').attr('src', '').hide();
+      $('#modal_crop_update').modal('hide');
+      loadCropCodeToModel();
+      loadCropDetailsTable();
+    },
+    error: function(error) {
+      alert(error.responseText);
+    }
+  });
+});
+  
