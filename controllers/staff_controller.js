683333f7c1b5506d267ce284fbd44e4133c018f9
pathumck <ckpathumck@gmail.com>
2024-12-01 23:29:52 +0530

    create load staff to table

--- controllers/staff_controller.js
+++ controllers/staff_controller.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   loadStaffIdToModel();
+  loadStaffDetailsTable();
 });
 
 function loadStaffIdToModel() {
@@ -64,3 +65,58 @@ $('#btn_save_staff').click(function() {
     }  
   });
 });
+
+function loadStaffDetailsTable() {
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/staff',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      const tableBody = $('#staff_details_table_body');
+      tableBody.empty();
+      data.data.forEach(data => {
+        const row = `
+          <tr>
+            <td>${data.staffId}</td>
+            <td>${data.firstName}</td>
+            <td>${data.lastName}</td>
+            <td>${data.joinDate}</td>
+            <td>${data.birthDate}</td>
+            <td>${data.gender}</td>
+            <td>${data.phone}</td>
+            <td>${data.email}</td>
+            <td>${data.designation}</td>
+            <td>${data.role}</td>
+            <td>${data.addressLine1}</td>
+            <td>${data.addressLine2}</td>
+            <td>${data.addressLine3}</td>
+            <td>${data.addressLine4}</td>
+            <td>${data.addressLine5}</td>
+            <td><button type="button" class="btn btn-primary btn-update-staff" data-bs-toggle="modal" data-bs-target="#modal_staff_update"
+            data-staff-id="${data.staffId}"
+            data-first-name="${data.firstName}"
+            data-last-name="${data.lastName}"
+            data-joined-date="${data.joinDate}"
+            data-birth-date="${data.birthDate}"
+            data-gender="${data.gender}"
+            data-phone="${data.phone}"
+            data-email="${data.email}"
+            data-designation="${data.designation}"            
+            data-role="${data.role}"
+            data-address1="${data.addressLine1}"
+            data-address2="${data.addressLine2}"
+            data-address3="${data.addressLine3}"
+            data-address4="${data.addressLine4}"
+            data-address5="${data.addressLine5}"
+            >Update</button></td>
+            <td><button type="button" class="btn btn-danger btn-delete-staff" data-staff-id="${data.staffId}">Delete</button></td>            
+          </tr>
+        `;
+        tableBody.append(row);
+      });
+    },
+    error: function (error) { 
+      alert(error.responseText);
+    }  
+  });
+}
