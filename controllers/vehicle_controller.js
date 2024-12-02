a5887b487b80e7b775166fdd9587f0cf85658299
pathumck <ckpathumck@gmail.com>
2024-12-02 12:31:41 +0530

    create update vehicle

--- controllers/vehicle_controller.js
+++ controllers/vehicle_controller.js
@@ -111,7 +111,15 @@ function loadVehicleDetailsTable() {
           <td>${data.status}</td>
           <td>${data.remarks}</td>
           <td>${data.staffId}</td>
-          <td><button class="btn btn-primary btn-update-vehicle" onclick="loadUpdateVehicleModal('${data.vehicleCode}')">Update</button></td>
+          <td><button type="button" class="btn btn-primary btn-update-vehicle" data-bs-toggle="modal" data-bs-target="#vehicle_modal_update"
+            data-vehicle-code="${data.vehicleCode}"
+            data-vehicle-number="${data.vehicleNumber}"
+            data-vehicle-category="${data.vehicleCategory}"
+            data-fuel-type="${data.fuelType}"
+            data-status="${data.status}"
+            data-remarks="${data.remarks}"
+            data-staff-id="${data.staffId}"
+            >Update</button></td>
           <td><button type="button" class="btn btn-danger btn-delete-vehicle" data-vehicle-code="${data.vehicleCode}">Delete</button></td>   
         </tr>`;
         tableBody.append(row);
@@ -120,6 +128,11 @@ function loadVehicleDetailsTable() {
       $('.btn-delete-vehicle').click(function () {
         const vehicleCode = $(this).data('vehicle-code');
         deleteVehicle(vehicleCode);
+      });
+
+      $('.btn-update-vehicle').click(function () {
+        const vehicleCode = $(this).data('vehicle-code');
+        loadUpdateVehicleModal(vehicleCode);
       });
     }
   });
@@ -141,4 +154,90 @@ function deleteVehicle(vehicleCode) {
       }
     });
   }
+}
+
+function loadUpdateVehicleModal(vehicleCode) {
+  console.log(vehicleCode);
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/vehicle/' + vehicleCode,
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      $('#code_modal_vehicle_update').text(data.data.vehicleCode);
+      $('#vehicleNumber_modal_vehicle_update').val(data.data.vehicleNumber);
+      $('#category_modal_vehicle_update').val(data.data.vehicleCategory);
+      $('#fuelType_modal_vehicle_update').val(data.data.fuelType);
+      $('#status_modal_vehicle_update').val(data.data.status);
+      $('#remarks_modal_vehicle_update').val(data.data.remarks);
+      loadStaffIdToUpdateVehicleModal(data.data.staffId);
+      loadStaffIdsToVehicleUpdateModal();
+      $('#modal_vehicle_update').modal('show');
+    },
+    error: function (error) {
+      alert(error.responseText);
+    }
+  });
+}
+
+$('#btn_update_vehicle').click(function () {
+  const vehicleCode = $('#code_modal_vehicle_update').text();
+  const vehicle = {
+    vehicleCode: vehicleCode,
+    vehicleNumber: $('#vehicleNumber_modal_vehicle_update').val(),
+    vehicleCategory: $('#category_modal_vehicle_update').val(),
+    fuelType: $('#fuelType_modal_vehicle_update').val(),
+    status: $('#status_modal_vehicle_update').val(),
+    remarks: $('#remarks_modal_vehicle_update').val(),
+    staffId: $('#staffId_modal_vehicle_update').val(),
+  };
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/vehicle', 
+    type: 'PUT',
+    data: JSON.stringify(vehicle),  
+    contentType: 'application/json',
+    processData: false,
+    success: function (data) {
+      loadVehicleDetailsTable();
+      loadVehicleIdToModel();
+      alert(vehicleCode + " : Vehicle updated successfully!");  
+      $('#vehicle_modal_update').modal('hide');
+    },
+    error: function (error) {
+      alert(error.responseText);
+    }
+  });
+});
+
+function loadStaffIdToUpdateVehicleModal(staffId) {
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/staff/' + staffId,
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      const dropDown = $('#staffId_modal_vehicle_update');
+      dropDown.empty();
+      dropDown.append(`<option value="${data.data.staffId}">${data.data.staffId} ${data.data.firstName} ${data.data.lastName}</option>`);
+    },
+    error: function (error) {
+      alert(error.responseText);
+    }
+  });
+}
+
+function loadStaffIdsToVehicleUpdateModal() {
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/staff',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      const dropDown = $('#staffId_modal_vehicle_update');
+      dropDown.append(`<option value=""disabled>Select Staff</option>`);
+      data.data.forEach(data => {
+        const option = `<option value="${data.staffId}">${data.staffId} ${data.firstName} ${data.lastName}</option>`;
+        if (data.staffId !== $('#staffId_modal_vehicle_update').val()) {
+          dropDown.append(option);
+        }
+      });
+    }
+  });
 }
