904c0b2ec788c8931e2ec0201584bd01b4553eab
pathumck <ckpathumck@gmail.com>
2024-12-05 10:56:44 +0530

    create add crops

--- controllers/create_log_controller.js
+++ controllers/create_log_controller.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   loadDateToLabel();
   setInterval(loadDateToLabel, 1000);
   selectFieldCodeForCreateLog();
+  selectCropCodeForCreateLog();
 });
 
 function loadLogIdToLabel() {
@@ -55,7 +56,6 @@ function selectFieldCodeForCreateLog() {
 
         const imageOneSrc = `data:image/jpeg;base64,${imageOneBase64}`;
         const imageTwoSrc = `data:image/jpeg;base64,${imageTwoBase64}`;
-        console.log(imageOneSrc);
 
         $('#lbl_cl_name').text(name);
         $('#lbl_cl_location').text(location);
@@ -107,3 +107,85 @@ function selectFieldCodeForCreateLog() {
   });
 }
 
+function selectCropCodeForCreateLog() {
+  $.ajax({
+    url: 'http://localhost:8080/greenshadow/crop',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      const dropDown = $('#sel_cl_cropCode');
+      dropDown.empty();
+      dropDown.append(`<option value="" selected disabled>Select Crop</option>`);
+      data.data.forEach(data => {
+        const option = `<option 
+        data-cropCode="${data.cropCode}"
+        data-commonName="${data.commonName}"
+        data-scientificName="${data.scientificName}"
+        data-category="${data.category}"
+        data-season="${data.season}"
+        data-image="${data.image}"
+        value="${data.cropCode}">${data.cropCode} ${data.commonName} ${data.scientificName}</option>`;
+        dropDown.append(option);
+      });
+
+      $('#sel_cl_cropCode').on('change', function() {
+        const selectedOption = $(this).find(':selected');
+        const commonName = selectedOption.attr('data-commonName');
+        const scientificName = selectedOption.attr('data-scientificName');
+        const category = selectedOption.data('category');
+        const season = selectedOption.data('season');
+        const imageBase64 = selectedOption.attr('data-image');
+
+        const imageSrc = `data:image/jpeg;base64,${imageBase64}`;
+
+        $('#lbl_cl_commonName').text(commonName);
+        $('#lbl_cl_scientificName').text(scientificName);
+        $('#lbl_cl_category').text(category);
+        $('#lbl_cl_season').text(season);
+
+        $('#img_cl_image').attr('src', imageSrc).show();
+      });
+
+      $('#btn_cl_add_crop').on('click', function() {
+        const selectedOption = $('#sel_cl_cropCode').find(':selected');
+        if (!selectedOption.val()) {
+          alert('Please select a crop.');
+          return;
+        }
+
+        const cropCode = selectedOption.attr('data-cropCode');
+        const commonName = selectedOption.attr('data-commonName');
+        const scientificName = selectedOption.attr('data-scientificName');
+        const category = selectedOption.data('category');
+        const season = selectedOption.data('season');
+        const imageBase64 = selectedOption.attr('data-image');
+
+        const imageSrc = `data:image/jpeg;base64,${imageBase64}`;
+
+        const isDuplicate = $('#tbody_cl_crops tr').filter(function() {
+          return $(this).find('td').eq(0).text() === cropCode;
+        }).length > 0;
+
+        if (isDuplicate) {
+          alert('This crop ' + cropCode + ' is already added.');
+          return;
+        }
+        const tableRow = `
+          <tr>
+            <td>${cropCode}</td>
+            <td>${commonName}</td>
+            <td>${scientificName}</td>
+            <td>${category}</td>
+            <td>${season}</td>
+            <td><img src="${imageSrc}" alt="Image" width="70"></td>
+          </tr>
+        `;
+
+        $('#tbody_cl_crops').append(tableRow);
+      });
+    },
+    error: function(error) {
+      alert(error.responseText);
+    }
+  });
+}
